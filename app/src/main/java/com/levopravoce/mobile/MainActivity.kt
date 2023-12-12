3267519bf308f311a2e8e4b795db42640f59f188
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-12-12 00:52:15 -0300

    feat: create home component for testing navigation to auth

--- app/src/main/java/com/levopravoce/mobile/MainActivity.kt
+++ app/src/main/java/com/levopravoce/mobile/MainActivity.kt
@@ -5,52 +5,38 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.levopravoce.mobile.features.auth.representation.authGraph
 import com.levopravoce.mobile.ui.theme.MobileTheme
 import com.levopravoce.mobile.ui.theme.customColorsShema
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 @ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MobileTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting(name = "teste")
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "home") {
+                    authGraph(navController = navController)
+                    composable("home") {
+                        Column(modifier = Modifier.clickable {
+                            navController.navigate("auth")
+                        }) {
+                            Text(text = "Home")
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column (Modifier.clickable {
-
-    }) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier,
-            color = MaterialTheme.customColorsShema.background
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MobileTheme {
-        Greeting("Android")
     }
 }
