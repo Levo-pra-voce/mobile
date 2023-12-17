0fdc73c2ad125269f8fb23501f1cf54af932ebdf
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-12-17 14:07:39 -0300

    feat: add builder for mount api with custom interface

---
+++ app/src/main/java/com/levopravoce/mobile/config/ApiClient.kt
@@ -0,0 +1,36 @@
+package com.levopravoce.mobile.config
+
+import com.levopravoce.mobile.features.auth.domain.AuthInterceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ApiClient @Inject constructor(
+    private val authInterceptor: AuthInterceptor
+) {
+    fun <T> buildApi(
+        api: Class<T>
+    ): T {
+        val client = OkHttpClient.Builder()
+            .addInterceptor {
+                val request = it.request().newBuilder()
+                    .addHeader("Accept", "application/json")
+                    .addHeader("Content-Type", "application/json")
+                    .build()
+                it.proceed(request)
+            }
+            .addInterceptor(authInterceptor)
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.levopravoce.com")
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        return retrofit.create(api)
+    }
+}
