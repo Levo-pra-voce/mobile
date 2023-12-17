1ac46ff162cd33253faa2f2faf48af9e82cdf0e1
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-12-17 14:07:11 -0300

    feat: add global module

---
+++ app/src/main/java/com/levopravoce/mobile/features/app/AppModule.kt
@@ -0,0 +1,23 @@
+package com.levopravoce.mobile.features.app
+
+import android.content.Context
+import com.levopravoce.mobile.features.auth.domain.AuthStore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideAuthStore(
+        @ApplicationContext appContext: Context
+    ): AuthStore {
+        return AuthStore(appContext)
+    }
+}
