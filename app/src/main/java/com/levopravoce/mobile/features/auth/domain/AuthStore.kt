e9fef555256fdaf40230a9e52b3f0b66c6e53cc2
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-12-17 02:11:07 -0300

    feat: create store for auth

---
+++ app/src/main/java/com/levopravoce/mobile/features/auth/domain/AuthStore.kt
@@ -0,0 +1,27 @@
+package com.levopravoce.mobile.features.auth.domain
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class AuthStore(private val context: Context) {
+    companion object {
+        private val Context.dataStore: DataStore<Preferences> by preferencesDataStore("auth")
+        private val TOKEN_KEY = stringPreferencesKey("jwt")
+    }
+
+    val getAccessToken: Flow<String?> = context.dataStore.data.map { preferences ->
+        preferences[TOKEN_KEY]
+    }
+
+    suspend fun saveToken(token: String) {
+        context.dataStore.edit { preferences ->
+            preferences[TOKEN_KEY] = token
+        }
+    }
+}
