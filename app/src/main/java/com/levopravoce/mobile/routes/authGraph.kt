053ff7c90329820df73defd4dd84d23330557d29
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-12-18 14:24:06 -0300

    feat: create animation in change screen

--- app/src/main/java/com/levopravoce/mobile/routes/authGraph.kt
+++ app/src/main/java/com/levopravoce/mobile/routes/authGraph.kt
@@ -1,15 +1,56 @@
 package com.levopravoce.mobile.routes
 
-import androidx.navigation.NavController
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import com.levopravoce.mobile.features.login.representation.Login
+import com.levopravoce.mobile.features.register.representation.Register
+import com.levopravoce.mobile.features.start.representation.Start
 
-fun NavGraphBuilder.authGraph(navController: NavController) {
-    navigation(startDestination = "login", route = "auth") {
-        composable("login") {
+fun NavGraphBuilder.authGraph() {
+    navigation(startDestination = Routes.Auth.START, route = Routes.Auth.ROUTE) {
+        composable(
+            route = Routes.Auth.LOGIN,
+            enterTransition = {
+                slideIntoContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Left,
+                    animationSpec = tween(700)
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Right,
+                    animationSpec = tween(700)
+                )
+            },
+        ) {
             Login()
+        }
+
+        composable(
+            route = Routes.Auth.START,
+        ) {
+            Start()
+        }
+
+        composable(
+            route = Routes.Auth.REGISTER,
+            enterTransition = {
+                slideIntoContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Right,
+                    animationSpec = tween(700)
+                )
+            },
+            exitTransition = {
+                slideOutOfContainer(
+                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Left,
+                    animationSpec = tween(700)
+                )
+            },
+        ) {
+            Register()
         }
     }
 }
