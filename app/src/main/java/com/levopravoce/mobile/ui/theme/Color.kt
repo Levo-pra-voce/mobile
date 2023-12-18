3a919d1d761189a92e81530f8858b13cc9b916eb
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-12-18 13:55:17 -0300

    feat: add new colors

--- app/src/main/java/com/levopravoce/mobile/ui/theme/Color.kt
+++ app/src/main/java/com/levopravoce/mobile/ui/theme/Color.kt
@@ -4,5 +4,8 @@ import androidx.compose.ui.graphics.Color
 
 val Black100 = Color(0xFF000000)
 val Black80 = Color(0xFF1A1A1A)
-val White100 = Color(0xFFF5F6F8)
+val gray80 = Color(0xFF979797)
+val Black60 = Color(0xEDF6FFFF)
+val White100 = Color(0xFFEDF6FF)
 val White80 = Color(0xFFEDF6FF)
+val White60 = Color(0x99EDF6FF)
