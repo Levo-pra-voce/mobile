c7e2d55686afda8ad5df68a84dc60af2ef326725
luiz-eduardo14 <masterdudu123@hotmail.com>
2024-01-14 17:07:58 -0300

    feat: change singleton for inject AuthRepository

--- app/src/main/java/com/levopravoce/mobile/features/auth/AuthModule.kt
+++ app/src/main/java/com/levopravoce/mobile/features/auth/AuthModule.kt
@@ -5,12 +5,14 @@ import com.levopravoce.mobile.features.auth.data.AuthRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(SingletonComponent::class)
 object AuthModule {
     @Provides
+    @Singleton
     fun provideAuthRepository(
         apiClient: ApiClient
     ): AuthRepository {
