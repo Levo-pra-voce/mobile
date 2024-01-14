f990d775fff020b3647542f96e1533650367ac16
luiz-eduardo14 <masterdudu123@hotmail.com>
2024-01-14 18:31:58 -0300

    feat: moving loading composable function to common use in all modules

--- app/src/main/java/com/levopravoce/mobile/features/app/representation/Loading.kt
+++ app/src/main/java/com/levopravoce/mobile/features/app/representation/Loading.kt
@@ -1,32 +1,29 @@
-package com.levopravoce.mobile.features.auth.representation
+package com.levopravoce.mobile.features.app.representation
 
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.levopravoce.mobile.ui.theme.customColorsShema
 
 @Composable
-fun AuthLoading() {
+fun Loading(
+    size: Dp = 92.dp
+) {
     val infiniteTransition = rememberInfiniteTransition(label = "")
 
     val rotateAnimation by infiniteTransition.animateFloat(
@@ -40,37 +37,28 @@ fun AuthLoading() {
         ), label = ""
     )
 
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
+    CircularProgressIndicator(
         modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .background(color = MaterialTheme.customColorsShema.background)
-    ) {
-        CircularProgressIndicator(
-            modifier = Modifier
-                .size(92.dp)
-                .rotate(rotateAnimation)
-                .border(
-                    width = 2.dp,
-                    brush = Brush.sweepGradient(
-                        listOf(
-                            MaterialTheme.customColorsShema.background,
-                            MaterialTheme.customColorsShema.border
-                        )
-                    ),
-                    shape = CircleShape
+            .size(size)
+            .rotate(rotateAnimation)
+            .border(
+                width = 2.dp,
+                brush = Brush.sweepGradient(
+                    listOf(
+                        MaterialTheme.customColorsShema.background,
+                        MaterialTheme.customColorsShema.border
+                    )
                 ),
-            progress = 1f,
-            strokeWidth = 1.dp,
-            color = MaterialTheme.customColorsShema.border
-        )
-    }
+                shape = CircleShape
+            ),
+        progress = 1f,
+        strokeWidth = 1.dp,
+        color = MaterialTheme.customColorsShema.border
+    )
 }
 
 @Composable
 @Preview(showBackground = true, showSystemUi = true)
 fun AuthLoadingPreview() {
-    AuthLoading()
+    Loading()
 }
