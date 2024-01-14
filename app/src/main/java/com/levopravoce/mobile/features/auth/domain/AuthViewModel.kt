d6910c82d997242a263fc6d9db15bb91ff5c7427
luiz-eduardo14 <masterdudu123@hotmail.com>
2024-01-14 17:10:16 -0300

    feat: remove test property of AuthUiState and create props for requests in application and create methods for logout in application, save token of authentication and login in application

--- app/src/main/java/com/levopravoce/mobile/features/auth/domain/AuthViewModel.kt
+++ app/src/main/java/com/levopravoce/mobile/features/auth/domain/AuthViewModel.kt
@@ -2,29 +2,70 @@ package com.levopravoce.mobile.features.auth.domain
 
 import androidx.lifecycle.ViewModel
 import com.levopravoce.mobile.features.auth.data.AuthRepository
+import com.levopravoce.mobile.features.auth.data.dto.UserDTO
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
-import javax.inject.Singleton
 
+enum class AuthRequestStatus {
+    IDLE,
+    LOADING,
+    SUCCESS,
+    ERROR
+}
 
 data class AuthUiState(
-    val test: String = ""
+    val data: UserDTO? = null,
+    val status: AuthRequestStatus = AuthRequestStatus.IDLE,
+    val error: String? = null
 )
-private val _uiState = MutableStateFlow(AuthUiState())
-val uiState: StateFlow<AuthUiState> = _uiState.asStateFlow()
 
-@Singleton
+
+@HiltViewModel
 class AuthViewModel @Inject constructor(
-    private val authRepository: AuthRepository
-): ViewModel() {
-    lateinit var uiState: AuthUiState
-    init {
+    private val authRepository: AuthRepository,
+    private val authStore: AuthStore
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(AuthUiState())
+
+    val uiState: StateFlow<AuthUiState> = _uiState
+
+    private suspend fun <T> requestProcessing(
+        vararg parameters: Any, request: suspend (
+            Array<out Any>
+        ) -> T
+    ) {
+        _uiState.value = AuthUiState(status = AuthRequestStatus.LOADING)
+        try {
+            val data = request(parameters);
+            _uiState.value = AuthUiState(data = data as UserDTO, status = AuthRequestStatus.SUCCESS)
+        } catch (
+            e: Exception
+        ) {
+            _uiState.value = AuthUiState(status = AuthRequestStatus.ERROR, error = e.message)
+        }
+    }
+
+    suspend fun logout() {
+        authStore.logout()
         _uiState.value = AuthUiState()
     }
 
-    fun meRequest() {
-        _uiState.value = AuthUiState()
+    suspend fun saveToken(token: String) {
+        authStore.saveToken(token)
+    }
+
+
+    suspend fun loginRequest(email: String, password: String) {
+        requestProcessing(email, password) {
+            authRepository.login(email, password)
+        }
+    }
+
+    suspend fun meRequest() {
+        requestProcessing {
+            authRepository.me()
+        }
     }
 }
