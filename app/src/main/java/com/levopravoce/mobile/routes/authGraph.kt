ceb541a34ce49f8487db26965f8a628f60355194
luiz-eduardo14 <masterdudu123@hotmail.com>
2024-01-14 21:57:33 -0300

    feat: create function for generate animation without copy and paste code

--- app/src/main/java/com/levopravoce/mobile/routes/authGraph.kt
+++ app/src/main/java/com/levopravoce/mobile/routes/authGraph.kt
@@ -1,7 +1,10 @@
 package com.levopravoce.mobile.routes
 
 import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.core.tween
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
@@ -9,22 +12,56 @@ import com.levopravoce.mobile.features.login.representation.Login
 import com.levopravoce.mobile.features.register.representation.Register
 import com.levopravoce.mobile.features.start.representation.Start
 
+const val DURATION_MILLIS = 700
+
+enum class SlideDirection {
+    Left, Right
+}
+
+fun generateSlideIntoContainerEnterTransition(
+    direction: SlideDirection,
+    durationMillis: Int
+): AnimatedContentTransitionScope<NavBackStackEntry>.() -> EnterTransition? = (
+    {
+        val slideDirection = when (direction) {
+            SlideDirection.Left -> AnimatedContentTransitionScope.SlideDirection.Left
+            SlideDirection.Right -> AnimatedContentTransitionScope.SlideDirection.Right
+        }
+        slideIntoContainer(
+            towards = slideDirection,
+            animationSpec = tween(durationMillis)
+        )
+    }
+)
+
+fun generateSlideIntoContainerExitTransition(
+    direction: SlideDirection,
+    durationMillis: Int
+): AnimatedContentTransitionScope<NavBackStackEntry>.() -> ExitTransition? = (
+    {
+        val slideDirection = when (direction) {
+            SlideDirection.Left -> AnimatedContentTransitionScope.SlideDirection.Left
+            SlideDirection.Right -> AnimatedContentTransitionScope.SlideDirection.Right
+        }
+        slideOutOfContainer(
+            towards = slideDirection,
+            animationSpec = tween(durationMillis)
+        )
+    }
+)
+
 fun NavGraphBuilder.authGraph() {
     navigation(startDestination = Routes.Auth.START, route = Routes.Auth.ROUTE) {
         composable(
             route = Routes.Auth.LOGIN,
-            enterTransition = {
-                slideIntoContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Left,
-                    animationSpec = tween(700)
-                )
-            },
-            exitTransition = {
-                slideOutOfContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Right,
-                    animationSpec = tween(700)
-                )
-            },
+            enterTransition = generateSlideIntoContainerEnterTransition(
+                direction = SlideDirection.Left,
+                durationMillis = DURATION_MILLIS
+            ),
+            exitTransition = generateSlideIntoContainerExitTransition(
+                direction = SlideDirection.Right,
+                durationMillis = DURATION_MILLIS
+            ),
         ) {
             Login()
         }
@@ -37,18 +74,14 @@ fun NavGraphBuilder.authGraph() {
 
         composable(
             route = Routes.Auth.REGISTER,
-            enterTransition = {
-                slideIntoContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Right,
-                    animationSpec = tween(700)
-                )
-            },
-            exitTransition = {
-                slideOutOfContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Companion.Left,
-                    animationSpec = tween(700)
-                )
-            },
+            enterTransition = generateSlideIntoContainerEnterTransition(
+                direction = SlideDirection.Left,
+                durationMillis = DURATION_MILLIS
+            ),
+            exitTransition = generateSlideIntoContainerExitTransition(
+                direction = SlideDirection.Left,
+                durationMillis = DURATION_MILLIS
+            ),
         ) {
             Register()
         }
