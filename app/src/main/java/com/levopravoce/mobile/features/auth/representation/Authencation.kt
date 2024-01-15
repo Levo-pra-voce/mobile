b0628843a2134e2c523847b844623e731b094835
luiz-eduardo14 <masterdudu123@hotmail.com>
2024-01-14 22:38:15 -0300

    feat: create custom style in loading data of meRequest in Authencation.kt

--- app/src/main/java/com/levopravoce/mobile/features/auth/representation/Authencation.kt
+++ app/src/main/java/com/levopravoce/mobile/features/auth/representation/Authencation.kt
@@ -1,13 +1,22 @@
 package com.levopravoce.mobile.features.auth.representation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.levopravoce.mobile.common.RequestStatus
+import com.levopravoce.mobile.features.app.representation.Loading
 import com.levopravoce.mobile.features.auth.domain.AuthViewModel
 import com.levopravoce.mobile.navControllerContext
 import com.levopravoce.mobile.routes.Routes
+import com.levopravoce.mobile.ui.theme.customColorsShema
 import kotlinx.coroutines.launch
 
 @Composable
@@ -47,7 +56,15 @@ fun Authencation(
         }
 
         RequestStatus.LOADING -> {
-//            AuthLoading()
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .background(color = MaterialTheme.customColorsShema.background),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Loading()
+            }
         }
     }
 }
