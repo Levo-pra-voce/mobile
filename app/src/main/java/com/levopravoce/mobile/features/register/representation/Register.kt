8457d289e2e9932ff1bedcf6b21eb27776bb18f0
luiz-eduardo14 <masterdudu123@hotmail.com>
2024-01-14 22:41:07 -0300

    feat: create register composable

---
+++ app/src/main/java/com/levopravoce/mobile/features/register/representation/Register.kt
@@ -0,0 +1,10 @@
+package com.levopravoce.mobile.features.register.representation
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun Register() {
+    Text(text = "Register")
+}
