72cddc831781221579cf4ecdb4cdf3b77a15a51f
luiz-eduardo14 <masterdudu123@hotmail.com>
2024-01-22 21:41:45 -0300

    feat: craete component for header in application

---
+++ app/src/main/java/com/levopravoce/mobile/features/app/representation/Header.kt
@@ -0,0 +1,31 @@
+package com.levopravoce.mobile.features.app.representation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.levopravoce.mobile.ui.theme.customColorsShema
+
+@Composable
+fun Header(
+    content: @Composable () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .background(color = MaterialTheme.customColorsShema.invertBackground)
+            .heightIn(min = 148.dp, max = 148.dp)
+            .padding(20.dp)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        content()
+    }
+}
