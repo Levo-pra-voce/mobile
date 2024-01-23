d579b3148bc047010e90afdd47eb352fd22bfc1f
luiz-eduardo14 <masterdudu123@hotmail.com>
2024-01-23 00:15:53 -0300

    feat: create data class Vehicle.kt

---
+++ app/src/main/java/com/levopravoce/mobile/features/auth/data/dto/Vehicle.kt
@@ -0,0 +1,8 @@
+package com.levopravoce.mobile.features.auth.data.dto
+
+data class Vehicle(
+    val plate: String? = null,
+    val model: String? = null,
+    val color: String? = null,
+    val vehicleType: VehicleType? = null
+)
