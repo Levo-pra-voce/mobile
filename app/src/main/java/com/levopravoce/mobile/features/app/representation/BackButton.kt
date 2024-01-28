a193e50d15777a52107d34a7d92b8f2de188612c
luiz-eduardo14 <masterdudu123@hotmail.com>
2024-01-27 21:49:30 -0300

    feat: create button for back to last route

---
+++ app/src/main/java/com/levopravoce/mobile/features/app/representation/BackButton.kt
@@ -0,0 +1,32 @@
+package com.levopravoce.mobile.features.app.representation
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.levopravoce.mobile.R
+import com.levopravoce.mobile.navControllerContext
+import com.levopravoce.mobile.ui.theme.customColorsShema
+
+@Composable
+fun BackButton(
+    modifier: Modifier = Modifier,
+) {
+
+    val navController = navControllerContext.current
+
+    Image(
+        painter = painterResource(id = R.drawable.baseline_arrow_back_16),
+        contentDescription = "Voltar",
+        colorFilter = ColorFilter.tint(MaterialTheme.customColorsShema.title),
+        contentScale = ContentScale.FillHeight,
+        modifier = modifier
+            .clickable {
+                navController.popBackStack()
+            }
+    )
+}
