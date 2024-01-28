98ecdd5d805ef2d04441aea99928ed55ccccaccb
luiz-eduardo14 <masterdudu123@hotmail.com>
2024-01-28 00:25:08 -0300

    feat: add RegisterDecider in Register.kt

--- app/src/main/java/com/levopravoce/mobile/features/register/representation/Register.kt
+++ app/src/main/java/com/levopravoce/mobile/features/register/representation/Register.kt
@@ -1,10 +1,17 @@
 package com.levopravoce.mobile.features.register.representation
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.levopravoce.mobile.features.auth.data.dto.UserType
 
 @Composable
 fun Register() {
-    Text(text = "Register")
+    var registerType by remember { mutableStateOf<UserType?>(null) }
+
+    if (registerType == null) {
+        RegisterDecider { registerType = it }
+    }
 }
