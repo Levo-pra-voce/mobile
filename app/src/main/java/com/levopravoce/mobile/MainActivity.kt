24048c082598c2fe1d88e565b617e45afd70db9e
luiz-eduardo14 <masterdudu123@hotmail.com>
2024-01-30 00:20:15 -0300

    feat: apply CompositionNavHost in MainActivity.kt

--- app/src/main/java/com/levopravoce/mobile/MainActivity.kt
+++ app/src/main/java/com/levopravoce/mobile/MainActivity.kt
@@ -4,18 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.compositionLocalOf
-import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
-import com.levopravoce.mobile.routes.Routes
-import com.levopravoce.mobile.routes.authGraph
-import com.levopravoce.mobile.routes.homeGraph
+import com.levopravoce.mobile.routes.CompositionNavHost
 import com.levopravoce.mobile.ui.theme.MobileTheme
 import dagger.hilt.android.AndroidEntryPoint
-
-val navControllerContext = compositionLocalOf<NavController> { error("No NavController found!") }
 
 @ExperimentalMaterial3Api
 @AndroidEntryPoint
@@ -24,16 +15,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MobileTheme {
-                val navHost = rememberNavController()
-                CompositionLocalProvider(navControllerContext provides navHost) {
-                    NavHost(
-                        navController = navHost,
-                        startDestination = Routes.Home.ROUTE
-                    ) {
-                        authGraph()
-                        homeGraph()
-                    }
-                }
+                CompositionNavHost()
             }
         }
     }
