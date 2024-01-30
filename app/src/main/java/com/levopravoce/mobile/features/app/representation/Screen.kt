7e2ae763265b864dd35f05106587c32c9d82e127
luiz-eduardo14 <masterdudu123@hotmail.com>
2024-01-30 00:21:55 -0300

    feat: create component for build screen with custom background

---
+++ app/src/main/java/com/levopravoce/mobile/features/app/representation/Screen.kt
@@ -0,0 +1,28 @@
+package com.levopravoce.mobile.features.app.representation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.levopravoce.mobile.ui.theme.customColorsShema
+
+@Composable
+fun Screen(
+    content: @Composable () -> Unit
+) {
+    Column(
+        Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .background(color = MaterialTheme.customColorsShema.background)
+    ) {
+        Column(Modifier.padding(16.dp)) {
+            content()
+        }
+    }
+}
