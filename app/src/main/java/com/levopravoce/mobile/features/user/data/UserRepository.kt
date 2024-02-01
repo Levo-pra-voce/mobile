843e791acf38e007fb70638dc8bd594513681b39
luiz-eduardo14 <masterdudu123@hotmail.com>
2024-01-31 22:42:20 -0300

    feat: transform request register to POST

--- app/src/main/java/com/levopravoce/mobile/features/user/data/UserRepository.kt
+++ app/src/main/java/com/levopravoce/mobile/features/user/data/UserRepository.kt
@@ -3,12 +3,12 @@ package com.levopravoce.mobile.features.user.data
 import com.levopravoce.mobile.features.auth.data.dto.JwtResponseDTO
 import com.levopravoce.mobile.features.auth.data.dto.UserDTO
 import retrofit2.http.Body
-import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface UserRepository {
 
-    @GET("/api/auth/register/{userType}")
+    @POST("/api/auth/register/{userType}")
     suspend fun register(
         @Path("userType") userType: String,
         @Body user: UserDTO
