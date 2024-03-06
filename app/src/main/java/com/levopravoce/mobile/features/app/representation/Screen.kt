79811f67cfc7ad8c257f47cb48f18ebbd2c509d3
luiz-eduardo14 <masterdudu123@hotmail.com>
2024-03-06 01:43:51 -0300

    feat: add vertical align and horizontal align in Screen.kt

--- app/src/main/java/com/levopravoce/mobile/features/app/representation/Screen.kt
+++ app/src/main/java/com/levopravoce/mobile/features/app/representation/Screen.kt
@@ -1,19 +1,25 @@
 package com.levopravoce.mobile.features.app.representation
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.levopravoce.mobile.ui.theme.customColorsShema
 
 @Composable
 fun Screen(
     modifier: Modifier = Modifier,
+    verticalArrangement: Arrangement.Vertical = Arrangement.Top,
+    horizontalAlignment: Alignment.Horizontal = Alignment.Start,
+    padding: Dp? = null,
     content: @Composable () -> Unit
 ) {
     Column(
@@ -21,9 +27,11 @@ fun Screen(
             .fillMaxHeight()
             .fillMaxWidth()
             .background(color = MaterialTheme.customColorsShema.background)
-            .then(modifier)
+            .then(modifier),
+        verticalArrangement,
+        horizontalAlignment
     ) {
-        Column(Modifier.padding(16.dp)) {
+        Column(Modifier.padding(padding ?:16.dp)) {
             content()
         }
     }
