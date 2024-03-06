d6b3340a924b6f54cc32028ba8879822e55f69fc
luiz-eduardo14 <masterdudu123@hotmail.com>
2024-03-06 01:43:17 -0300

    feat: add callback in WebSocketClient.kt

--- app/src/main/java/com/levopravoce/mobile/config/WebSocketClient.kt
+++ app/src/main/java/com/levopravoce/mobile/config/WebSocketClient.kt
@@ -1,13 +1,17 @@
 package com.levopravoce.mobile.config
 
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.levopravoce.mobile.BuildConfig
 import com.levopravoce.mobile.features.app.data.dto.MessageSocketDTO
 import com.levopravoce.mobile.features.auth.domain.AuthStore
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
@@ -25,8 +29,6 @@ private data class Event(
 class WebSocketEventDTO(headers: Map<String, String>, body: String?) {
     val headers: Map<String, String>? = null
     val body: String? = null
-    val isValid: Boolean
-        get() = headers != null && headers.containsKey("destination") && body != null
 
     enum class HEADERS(
         val value: String
@@ -45,7 +47,8 @@ class WebSocketClient @Inject constructor(
 ) {
     private var webSocket: WebSocket? = null
     private var isConnected: Boolean = false
-    private val gson = Gson()
+    private val gson = GsonBuilder().serializeNulls().create();
+    var callback: ((MessageSocketDTO) -> Unit)? = null
 
     val messagesFlow = MutableStateFlow<MessageSocketDTO?>(null);
 
@@ -57,12 +60,16 @@ class WebSocketClient @Inject constructor(
         }
 
         override fun onMessage(webSocket: WebSocket, text: String) {
+            super.onMessage(webSocket, text)
             try {
-                messagesFlow.value = gson.fromJson(text, MessageSocketDTO::class.java)
+                runBlocking(Dispatchers.IO) {
+                    val message = gson.fromJson(text, MessageSocketDTO::class.java)
+                    messagesFlow.emit(message)
+                    callback?.invoke(message)
+                }
             } catch (e: Exception) {
                 e.printStackTrace()
             }
-            super.onMessage(webSocket, text)
         }
 
         override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
