b39286b997ff2df37d3b9ee8aac9ff16e54e103b
luiz-eduardo14 <masterdudu123@hotmail.com>
2024-03-08 00:58:39 -0300

    feat: apply send icon in ChatBar.kt

--- app/src/main/java/com/levopravoce/mobile/features/chat/representation/ChatBar.kt
+++ app/src/main/java/com/levopravoce/mobile/features/chat/representation/ChatBar.kt
@@ -1,16 +1,13 @@
 package com.levopravoce.mobile.features.chat.representation
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDp
-import androidx.compose.animation.core.spring
-import androidx.compose.animation.core.updateTransition
+import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -19,12 +16,12 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,23 +29,13 @@ import androidx.compose.ui.unit.dp
 import com.levopravoce.mobile.R
 import com.levopravoce.mobile.ui.theme.customColorsShema
 
+private const val ICON_OFFSET = 16
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ChatBar(
     currentMessageState: MutableState<String>
 ) {
-
-    val transition = updateTransition(
-        targetState = currentMessageState.value.isNotEmpty(),
-        label = "transição do botão da camera"
-    )
-
-    val cameraIconOffset by transition.animateDp(
-        transitionSpec = { spring(stiffness = Spring.StiffnessLow) },
-        label = "Camera Icon Offset"
-    ) {
-        if (it) 32.dp else 24.dp
-    }
 
     Row(
         Modifier
@@ -81,20 +68,42 @@ fun ChatBar(
                 focusedLabelColor = Color.Red,
                 focusedLeadingIconColor = Color.Red,
             ),
+            modifier = Modifier
+                .width(300.dp)
         )
-        AnimatedVisibility(visible = currentMessageState.value.isEmpty()) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 12.dp)
-                    .offset(x = cameraIconOffset)
-            ) {
-                Image(
-                    painter = painterResource(R.drawable.cam_icon),
-                    contentDescription = "icone da camera",
-                    contentScale = ContentScale.FillHeight,
-                )
+        Crossfade(
+            targetState = currentMessageState.value.isNotEmpty(),
+            label = "crossfade do botão da camera"
+        ) {
+            if (it) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 12.dp)
+                        .offset(x = ICON_OFFSET.dp)
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.send_icon),
+                        contentDescription = "icone da camera",
+                        contentScale = ContentScale.FillHeight,
+                        colorFilter = ColorFilter.tint(MaterialTheme.customColorsShema.title),
+                    )
+                }
+            } else {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 12.dp)
+                        .offset(x = ICON_OFFSET.dp)
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.cam_icon),
+                        contentDescription = "icone da camera",
+                        contentScale = ContentScale.FillHeight,
+                    )
+                }
             }
+
         }
     }
 }
