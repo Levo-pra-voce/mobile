dcc3696b3fae98eebb6b6ca7ad40fd17087731ab
Gabriel V <tr00vuada@localhost.localdomain>
2024-06-08 21:49:56 -0300

    chore: problens in the order

---
+++ app/src/main/java/com/levopravoce/mobile/features/order/data/dto/GoogleDistanceMatrixRequestDTO.kt
@@ -0,0 +1,9 @@
+package com.levopravoce.mobile.features.order.data.dto
+
+
+class GoogleDistanceMatrixRequestDTO {
+    private val originLat: Double? = null
+    private val originLng: Double? = null
+    private val destinationLat: Double? = null
+    private val destinationLng: Double? = null
+}
