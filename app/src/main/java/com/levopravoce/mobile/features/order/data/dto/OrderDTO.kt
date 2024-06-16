bb1bc4459122ee2dae7038f84c458025a5b9c363
Tr00vuada <gabrielmiguel651@gmail.com>
2024-06-16 04:00:51 -0300

    feat: add new fields to OrderDTO

--- app/src/main/java/com/levopravoce/mobile/features/order/data/dto/OrderDTO.kt
+++ app/src/main/java/com/levopravoce/mobile/features/order/data/dto/OrderDTO.kt
@@ -14,6 +14,14 @@ data class OrderDTO (
     var status: OrderStatus? = null,
     var value: Double? = null,
     var deliveryman: UserDTO? = null,
-    var client: UserDTO? = null, //    private VehicleDTO vehicle;
+    var client: UserDTO? = null,
+    var averageRating: Double? = null,
+    var destinationLatitude: Double? = null,
+    var destinationLongitude: Double? = null,
+    var originLatitude: Double? = null,
+    var originLongitude: Double? = null,
+    var destinationAddress: String? = null,
+    var originAddress: String? = null,
+    //    private VehicleDTO vehicle;
     //    private PaymentDTO payment;
 )
