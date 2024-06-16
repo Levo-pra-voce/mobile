0b005842dd8c7a8ca64fa6955a2bfedd77a8a270
Tr00vuada <gabrielmiguel651@gmail.com>
2024-06-16 03:59:44 -0300

    feat: create new endpoint call for OrderRepository.kt

--- app/src/main/java/com/levopravoce/mobile/features/order/data/OrderRepository.kt
+++ app/src/main/java/com/levopravoce/mobile/features/order/data/OrderRepository.kt
@@ -2,7 +2,6 @@ package com.levopravoce.mobile.features.order.data
 
 import com.levopravoce.mobile.features.auth.data.dto.JwtResponseDTO
 import com.levopravoce.mobile.features.auth.data.dto.UserDTO
-import com.levopravoce.mobile.features.order.data.dto.GoogleDistanceMatrixRequestDTO
 import com.levopravoce.mobile.features.order.data.dto.OrderDTO
 import retrofit2.Response
 import retrofit2.http.Body
@@ -12,7 +11,6 @@ import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface OrderRepository {
 
@@ -22,13 +20,11 @@ interface OrderRepository {
         @Body user: OrderDTO
     ): Response<OrderDTO>
 
-    @GET("/api/map/distance")
-    suspend fun getDistance(
-        @Query("originLat") originLat: Double,
-        @Query("originLng") originLng: Double,
-        @Query("destinationLat") destinationLat: Double,
-        @Query("destinationLng") destinationLng: Double
-    ): Response<GoogleDistanceMatrixRequestDTO>
+    @GET("/api/order/deliveries-pending")
+    suspend fun findAllPending(): List<OrderDTO>
 
-
+    @GET("/api/order/{id}")
+    suspend fun findById(
+        @Path("id") id: String
+    ): OrderDTO
 }
