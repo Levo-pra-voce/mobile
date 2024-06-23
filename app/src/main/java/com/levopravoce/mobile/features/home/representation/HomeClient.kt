5c9e5c3a01b383f213200a3a401b3112b6839106
Tr00vuada <gabrielmiguel651@gmail.com>
2024-06-23 19:37:06 -0300

    feat: add tracking watch for client home

--- app/src/main/java/com/levopravoce/mobile/features/home/representation/HomeClient.kt
+++ app/src/main/java/com/levopravoce/mobile/features/home/representation/HomeClient.kt
@@ -5,19 +5,31 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.levopravoce.mobile.R
 import com.levopravoce.mobile.common.viewmodel.hiltSharedViewModel
+import com.levopravoce.mobile.features.app.data.dto.ApiResponse
 import com.levopravoce.mobile.features.app.domain.MainViewModel
+import com.levopravoce.mobile.features.app.representation.Loading
+import com.levopravoce.mobile.features.app.representation.Screen
 import com.levopravoce.mobile.features.home.data.IconDescriptorData
+import com.levopravoce.mobile.features.order.data.dto.OrderDTO
+import com.levopravoce.mobile.features.order.data.dto.OrderStatus
+import com.levopravoce.mobile.features.tracking.domain.TrackingViewModel
 import com.levopravoce.mobile.routes.Routes
 import com.levopravoce.mobile.routes.navControllerContext
 import com.levopravoce.mobile.ui.theme.customColorsShema
@@ -28,15 +40,14 @@ private val firstLineDescriptorData = listOf(
     IconDescriptorData(
         id = R.drawable.delivery_ok,
         contentDescription = "icone para solicitar entrega",
-        title = "Solicitar\n" +
-                "entrega",
+        title = "Solicitar\n" + "entrega",
         route = Routes.Home.SELECT_ORDER
     ),
     IconDescriptorData(
         id = R.drawable.delivery_call,
         contentDescription = "icone para acompanhar entrega",
-        title = "Acompanhar\n" +
-                "entrega"
+        title = "Acompanhar\n" + "entrega",
+        route = Routes.Home.DELIVERY_TRACKING_CLIENT
     ),
 )
 
@@ -55,10 +66,51 @@ private val secondLineDescriptorData = listOf(
 )
 
 @Composable
-fun HomeClient() {
-    Column {
-        UserHeader()
-        UserOptions()
+fun HomeClient(
+    trackingViewModel: TrackingViewModel = hiltSharedViewModel()
+) {
+    val currentTrackingState by trackingViewModel.currentTrackingState.collectAsStateWithLifecycle()
+    val firstRenderState = trackingViewModel.firstRender.collectAsState()
+    val navController = navControllerContext.current
+    LaunchedEffect(Unit) { trackingViewModel.setStateCurrentTracking() }
+
+    LaunchedEffect(currentTrackingState) {
+        if (currentTrackingState is ApiResponse.Success || currentTrackingState is ApiResponse.Error) {
+            trackingViewModel.setFirstRender()
+        }
+    }
+
+    if (currentTrackingState is ApiResponse.Loading) {
+        Screen {
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .background(color = MaterialTheme.customColorsShema.background),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Loading()
+            }
+        }
+    } else {
+        val haveTracking = currentTrackingState is ApiResponse.Success && (currentTrackingState as ApiResponse.Success<OrderDTO?>).data != null
+        if (!haveTracking || !firstRenderState.value) {
+            Column {
+                UserHeader()
+                UserOptions()
+            }
+        } else {
+            val orderDTO = (currentTrackingState as ApiResponse.Success<OrderDTO?>).data
+            when (orderDTO?.status) {
+                OrderStatus.EM_PROGRESSO -> {
+                    navController?.navigate(Routes.Home.DELIVERY_TRACKING_CLIENT)
+                }
+                OrderStatus.ENTREGADO -> {
+                    navController?.navigate(Routes.Home.CLIENT_PAYMENT)
+                }
+                else -> {}
+            }
+        }
     }
 }
 
@@ -100,8 +152,7 @@ private fun UserOptions(
     val coroutineScope = rememberCoroutineScope()
 
     val thirdLineDescriptorData = listOf(
-        IconDescriptorData(
-            id = R.drawable.exit_icon,
+        IconDescriptorData(id = R.drawable.exit_icon,
             contentDescription = "icone para sair do aplicativo",
             title = "Sair",
             imageModifier = Modifier.offset(x = -(10.dp)),
@@ -109,8 +160,7 @@ private fun UserOptions(
                 coroutineScope.launch {
                     mainViewModel.logout()
                 }
-            }
-        ),
+            }),
         IconDescriptorData(
             id = R.drawable.configuration_icon,
             contentDescription = "icone para ver as configurações",
