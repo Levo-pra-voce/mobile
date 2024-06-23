a0d6c49a97b9ee13d5743182eac3d5eb053721ce
Tr00vuada <gabrielmiguel651@gmail.com>
2024-06-23 18:26:48 -0300

    feat: create new routes for delivery tracking and payment

--- app/src/main/java/com/levopravoce/mobile/routes/Routes.kt
+++ app/src/main/java/com/levopravoce/mobile/routes/Routes.kt
@@ -22,5 +22,9 @@ object Routes {
         val CHANGE_PASSWORD = "change_password"
         val DELIVERY_LIST = "delivery_list"
         val DELIVERY_DETAILS = "delivery/{deliveryId}"
+        val DELIVERY_TRACKING_CLIENT = "delivery_tracking/client"
+        val DELIVERY_TRACKING_DRIVER = "delivery_tracking/driver"
+        val DELIVERY_PAYMENT = "delivery_payment"
+        val CLIENT_PAYMENT = "client_payment"
     }
 }
