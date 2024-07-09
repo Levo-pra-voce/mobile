9445371cc1985922c390e7bd407a4d772964b142
luiz-eduardo14 <masterdudu123@hotmail.com>
2024-07-09 16:53:51 -0300

    feat: change default value to mockDelivery

--- app/src/main/java/com/levopravoce/mobile/features/user/representation/Register.kt
+++ app/src/main/java/com/levopravoce/mobile/features/user/representation/Register.kt
@@ -24,7 +24,7 @@ val mockClient = UserDTO(
 
 val mockDelivery = UserDTO(
     userType = UserType.ENTREGADOR,
-    name = "Luiz",
+    name = "Gabriel",
     zipCode = "88061412",
     complement = "Casa",
     cpf = "51604819073",
